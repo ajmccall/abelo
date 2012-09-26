f696ba4e38d3c44c032d3f1989cf4d3dcfc3df63
alasdair.mccall@gmail.com <alasdair.mccall@gmail.com>
2012-09-26 22:25:22 +0100

    pre AbeloMainView creation

---
+++ Abelo/AbeloMainView.h
@@ -0,0 +1,13 @@
+//
+//  AbeloMainView.h
+//  Abelo
+//
+//  Created by Alasdair McCall on 26/09/2012.
+//  Copyright (c) 2012 Alasdair McCall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AbeloMainView : UIView
+
+@end
---
+++ Abelo/AbeloMainView.m
@@ -0,0 +1,31 @@
+//
+//  AbeloMainView.m
+//  Abelo
+//
+//  Created by Alasdair McCall on 26/09/2012.
+//  Copyright (c) 2012 Alasdair McCall. All rights reserved.
+//
+
+#import "AbeloMainView.h"
+
+@implementation AbeloMainView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
